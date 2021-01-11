d13d73ca1022992456b3c1fd88a3786f386b15cf
MacroBiz <mbiscaini@gmail.com>
2021-01-11 19:03:48 +0000

    remote.exports for eqArray & test

--- eqArrays.js
+++ eqArrays.js
@@ -1,22 +1,10 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}.`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}.`);
-  }
-};
-// TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual("Samsies", "Samsies");
-// assertEqual(4, 13);
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
-  // this will first check if length is same and return false if not.
+  // this will first check if length is the same else return false.
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
@@ -24,12 +12,4 @@ const eqArrays = function(arr1, arr2) {
   } return true;
 };
 
-// eqArrays([1, 2, 3], [1, 2, 3]) // => true
-// eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 2], [1, 2, 3]), true); // => should FAIL
-assertEqual(eqArrays([1, 2,], [1, 2, 3]), true); // => should FAIL
+module.exports = eqArrays;
---
+++ test/eqArrayTest.js
@@ -0,0 +1,8 @@
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 2], [1, 2, 3]), true); // => should FAIL
+assertEqual(eqArrays([1, 2,], [1, 2, 3]), true); // => should FAIL
