71d2c7f6ffc473dc97eb1ce8963dc190d1f7c5b1
MacroBiz <mbiscaini@gmail.com>
2021-01-11 19:28:58 +0000

    remote.exports middle.js & test

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  // this will check every element in the arr to check if it is the same and return false if not.
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  } return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}.`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}.`);
-  }
-};
-
 const middle = function(midArr) {
   let arrLength = midArr.length;
   if (arrLength < 3) {
@@ -34,15 +14,6 @@ const middle = function(midArr) {
   }
 };
 
+module.exports = middle;
 
 // The middle function should return an array with only the middle element(s) of the provided array. This means that the length of the returned elements could vary.
-
-// For arrays with one or two elements, there is no middle. Return an empty array.
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-// For arrays with odd number of elements, an array containing a single middle element should be returned.
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-// For arrays with an even number of elements, an array containing the two elements in the middle should be returned
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
