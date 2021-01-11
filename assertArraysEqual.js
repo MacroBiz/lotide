6f985a2e6f0e07f856d790fac4d129a300f596d4
MacroBiz <mbiscaini@gmail.com>
2021-01-11 19:12:04 +0000

    module.exports for assertArraysEqual & test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  // this will check every element in the arr to check if it is the same and return false if not.
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  } return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
@@ -18,7 +8,4 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-assertArraysEqual([1, 2, 2], [1, 2, 3]); // => should FAIL
-assertArraysEqual([1, 2,], [1, 2, 3]); // => should FAIL
+module.exports = assertArraysEqual;
